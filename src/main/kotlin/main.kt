ad21c2ca8f097fb613d2d53ab7f9444bc9c3d320
Your Name <nafularespah@gmail.com>
2022-06-15 10:37:59 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val age = 25*365
+    val name="Respah"
+
+    println("Happy Birthday, $name!")
+
+    println(",,,,,,")
+    println("||||||")
+    println("=========")
+    println("@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("")
+    println  ("You are already $age days old, $name!")
+    println("$age are the best days to celebrate")
+    }
